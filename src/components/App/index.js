cdfdb1781923c083f73506e70eb3ed2d8ac5e2b1
Eugene Tsakh <onatolich@gmail.com>
2018-02-01 22:45:46 +0200

    Add base app component

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        Hello, World!
+      </div>
+    );
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-document.getElementById('root').innerText = 'Hello, World!';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
