c6b8b6f0fa034a3ac7292338e875dc137e8ad2a0
Eugene Tsakh <onatolich@gmail.com>
2018-02-01 23:41:11 +0200

    Display initial Modal inside the App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import Button from '../Button';
+import Modal from '../Modal';
 import './App.scss';
 
+const settingsMap = {
+  easy: {
+    grid: [10, 10],
+    mines: 10,
+  },
+  medium: {
+    grid: [20, 20],
+    mines: 50,
+  },
+  hard: {
+    grid: [40, 20],
+    mines: 100,
+  }
+};
+
 export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      settings: {},
+      inGame: false,
+      initialized: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ initialized: true });
+    });
+  }
+
+  startGame(settings) {
+    this.setState({
+      settings,
+      inGame: true,
+    });
+  }
+
+  getLevelClickHandler(level) {
+    return () => {
+      this.startGame(settingsMap[level]);
+    };
+  }
+
   render() {
+    const { initialized, inGame } = this.state;
+
     return (
       <div className="App">
-        Hello, World!
+        <Modal show={initialized && !inGame}>
+          <h1>Mine Sweeper V2</h1>
+          <p>
+            <Button onClick={this.getLevelClickHandler('easy')}>
+              Easy
+            </Button>
+            <Button onClick={this.getLevelClickHandler('medium')}>
+              Medium
+            </Button>
+            <Button onClick={this.getLevelClickHandler('hard')}>
+              Hard
+            </Button>
+          </p>
+        </Modal>
       </div>
     );
   }
